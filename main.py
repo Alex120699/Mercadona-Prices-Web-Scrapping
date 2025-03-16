40d6ac21b2f2fb0329c8e3d6532e5b6066dead7e
Alex120699 <alexcoronadosorli@gmail.com>
2025-03-16 17:06:28 +0100

    main solved

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from scripts.functions import get_mercadona_info
 
 def main():
@@ -6,9 +5,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-=======
-from code.functions import get_mercadona_info
-
-codigo_postal = int(input("Inserte codigo postal: "))
-get_mercadona_info(codigo_postal)
->>>>>>> 9013cf93f01714eda890b2850f3487cd1194ab1a
