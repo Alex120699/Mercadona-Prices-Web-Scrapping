866bb88d17457b3d48d8f23cd6fdd5eb218d1c5a
Alex120699 <alexcoronadosorli@gmail.com>
2025-03-17 14:12:56 +0100

    fixing bug on app

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import home as home
-import app.products as products
-import app.details as details
-import app.kpis as kpis
+import products as products
+import details as details
+import kpis as kpis
 
 # Sidebar para la navegación
 st.sidebar.title("Navegación")
