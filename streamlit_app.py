182d079110737cd7731ce616ef6cd0ee8c3c33fb
Alex120699 <alexcoronadosorli@gmail.com>
2025-03-31 00:27:51 +0200

    added chat pages (not working on prod)

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,11 @@ import app.detalle_producto as details
 import app.kpis as kpis
 import app.iva as iva
 import app.historico as historico
+import app.chat_v2 as chat
 
 # Sidebar para la navegación
 st.sidebar.title("Navegación")
-option = st.sidebar.radio("Selecciona una opción:", ["Home", "Ver productos", "Detalles de producto", "Cambios de Precios", "KPIs", "IVA Dashboards"])
+option = st.sidebar.radio("Selecciona una opción:", ["Home", "Ver productos", "Detalles de producto", "Cambios de Precios", "KPIs", "IVA Dashboards","Chat"])
 
 
 # Cargar el script según la opción seleccionada
@@ -24,3 +25,5 @@ elif option == "KPIs":
     kpis.show()
 elif option == "IVA Dashboards":
     iva.show()
+# elif option == "Chat":
+#     chat.show()
